d0bc8b78e544cd110d79b648abfbed2761483607
Chauhan-Shivam <cshivam828@gmail.com>
2024-03-20 16:33:21 +0530

    added settings menu

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -5,6 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject[] mainmenu;
+    [SerializeField] GameObject[] options;
+    [SerializeField] Transform cam;
+    [SerializeField] Transform settings_pos;
+    void Start()
+    {
+        for(int i=0;i<mainmenu.Length;i++)
+        {
+            mainmenu[i].SetActive(true);
+        }
+        for(int i=0;i<options.Length;i++)
+        {
+            options[i].SetActive(false);
+        }
+    }
     public void  StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
@@ -13,5 +28,28 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+    public void Settings()
+    {
+        //cam.position=settings_pos.position;
+        for(int i=0;i<mainmenu.Length;i++)
+        {
+            mainmenu[i].SetActive(false);
+        }
+        for(int i=0;i<options.Length;i++)
+        {
+            options[i].SetActive(true);
+        }
+    }
+    public  void BackToMainMenu()
+    {
+        for(int i=0;i<mainmenu.Length;i++)
+        {
+            mainmenu[i].SetActive(true);
+        }
+        for(int i=0;i<options.Length;i++)
+        {
+            options[i].SetActive(false);
+        }
     }
 }
